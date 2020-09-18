678e3e3f9a302cd61a4e07d750b6fa917e061501
Arun Purohit <rajpurohitarun97@gmail.com>
2020-09-18 16:32:05 +0530

    going to delete

--- osmod.js
+++ osmod.js
@@ -2,7 +2,7 @@ const os = require("os");
 const chalk = require('chalk');
 const validator = require("validator");
 
-
+//hello
 
 console.log(chalk.blue('Hello world!'));
 const res = validator.isEmail("Arun@purohitcom");
